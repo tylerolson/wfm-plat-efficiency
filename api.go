9fc135a81063e6d6d946434861c269a6c561c35c
Tyler Olson <tolson519@gmail.com>
2025-03-24 15:57:23 -0700

    add vendor list

--- api.go
+++ api.go
@@ -2,6 +2,8 @@ package main
 
 // From https://42bytes.notion.site/WFM-Api-v2-Documentation-5d987e4aa2f74b55a80db1a09932459d
 
+//v0.13.0
+
 type ItemsResponse struct {
 	ApiVersion string      `json:"apiVersion"`
 	Data       []ItemShort `json:"data"`
--- main.go
+++ main.go
@@ -3,24 +3,72 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 const API = "https://api.warframe.market/v2"
 
-func main() {
+type Program struct {
+	items map[string]ItemShort
+}
+
+func newProgram() *Program {
+	return &Program{items: make(map[string]ItemShort)}
+}
+
+func (p *Program) fetchItems() error {
 	resp, err := http.Get(fmt.Sprintf("%v/items", API))
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
 	decoder := json.NewDecoder(resp.Body)
+
 	var response ItemsResponse
 	err = decoder.Decode(&response)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	fmt.Println(response.ApiVersion)
+	for _, v := range response.Data {
+		p.items[v.Slug] = v
+	}
+
+	return nil
+}
+
+func (p *Program) getVendorItems(vendorList []string) []ItemShort {
+	var items = make([]ItemShort, 0)
+
+	if len(p.items) == 0 {
+		return nil
+	}
+
+	for _, v := range vendorList {
+		items = append(items, p.items[v])
+	}
+
+	return items
+}
+
+func main() {
+	// Create a logger
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+
+	slog.SetDefault(logger)
+
+	p := newProgram()
+	p.fetchItems()
+
+	hexis := p.getVendorItems(aribiterOfHexis)
+
+	slog.Info(
+		"yo",
+		"data", hexis,
+	)
+
 }
