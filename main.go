93ff16d610f7fe9ba32fbc7c1d9923aef39d09fb
Tyler Olson <tolson519@gmail.com>
2025-03-31 14:15:18 -0700

    add weighted price average and average volume

--- main.go
+++ main.go
@@ -11,39 +11,48 @@ import (
 
 const API = "https://api.warframe.market/v1"
 
+type ItemStat struct {
+	name             string
+	weightedAvgPrice float64
+	avgVol           float64
+}
+
 type Program struct {
-	items map[string]struct {
-		name            string
-		weightedAverage string
-	}
+	items map[string]ItemStat
 }
 
 func newProgram() *Program {
 	return &Program{
-		items: make(map[string]struct {
-			name            string
-			weightedAverage string
-		}),
+		items: make(map[string]ItemStat),
 	}
 }
 
 func (p *Program) getVendorStats(items []Item) error {
 	for _, v := range items {
-		err := getStatisitics(v)
+		price, vol, err := getStatisitics(v)
 		if err != nil {
 			return err
 		}
+
+		p.items[v.Name] = ItemStat{
+			name:             v.Name,
+			weightedAvgPrice: price,
+			avgVol:           vol,
+		}
+
+		slog.Info("found item", "name", v.Name, "weightedAvgPrice", price, "avgVol", vol)
 	}
 
 	return nil
 }
 
-func getStatisitics(item Item) error {
+// getStatisitics returns (weightedAveragePrice, averageVolume, error)
+func getStatisitics(item Item) (float64, float64, error) {
 	slog.Info("requesting", "name", item.Name, "type", item.Type)
 
 	resp, err := http.Get(fmt.Sprintf("%v/items/%v/statistics", API, item.Name))
 	if err != nil {
-		return fmt.Errorf("failed to get statistics:%w", err)
+		return 0, 0, fmt.Errorf("failed to get statistics:%w", err)
 	}
 	defer resp.Body.Close()
 
@@ -53,25 +62,25 @@ func getStatisitics(item Item) error {
 	var response map[string]any
 	err = decoder.Decode(&response)
 	if err != nil {
-		return fmt.Errorf("failed to decode statistics:%w", err)
+		return 0, 0, fmt.Errorf("failed to decode statistics:%w", err)
 	}
 
 	payload, ok := response["payload"].(map[string]any)
 	if !ok {
 		slog.Error("could not find payload")
-		return errors.New("could not find payload")
+		return 0, 0, errors.New("could not find payload")
 	}
 
 	statisticsClosed, ok := payload["statistics_closed"].(map[string]any)
 	if !ok {
 		slog.Error("could not find statistics_closed")
-		return errors.New("could not find statistics_closed")
+		return 0, 0, errors.New("could not find statistics_closed")
 	}
 
 	statistics90, ok := statisticsClosed["90days"].([]any)
 	if !ok {
 		slog.Error("could not find 90days")
-		return errors.New("could not find 90days")
+		return 0, 0, errors.New("could not find 90days")
 	}
 
 	// filter rank 0 mods when item is mod
@@ -82,13 +91,13 @@ func getStatisitics(item Item) error {
 			statMap, ok := v.(map[string]any)
 			if !ok {
 				slog.Error("could not assert statistic entry as map", "error", err)
-				return fmt.Errorf("could not assert statistic entry as map: %w", err)
+				return 0, 0, fmt.Errorf("could not assert statistic entry as map: %w", err)
 			}
 
-			modRank, ok := statMap["mod_rank"].(float64)
+			modRank, ok := statMap["mod_rank"].(float64) // json numbers are float64
 			if !ok {
 				slog.Error("could not find mod_rank", "error", err)
-				return fmt.Errorf("could not assert statistic entry as map: %w", err)
+				return 0, 0, fmt.Errorf("could not assert statistic entry as map: %w", err)
 			}
 
 			if modRank == 0 {
@@ -99,9 +108,18 @@ func getStatisitics(item Item) error {
 		statistics90 = mod0
 	}
 
-	slog.Info("90days", "90days", statistics90[:2])
+	today := statistics90[0].(map[string]any)
+	todayAvgPrice := today["avg_price"].(float64)
+	todayVolume := today["volume"].(float64)
 
-	return nil
+	yesterday := statistics90[1].(map[string]any)
+	yesterdayAvgPrice := yesterday["avg_price"].(float64)
+	yesterdayVolume := yesterday["volume"].(float64)
+
+	weightedAvgPrice := ((todayAvgPrice * todayVolume) + (yesterdayAvgPrice * yesterdayVolume)) / (todayVolume + yesterdayVolume)
+	avgVolume := (todayVolume + yesterdayVolume) / 2
+
+	return weightedAvgPrice, avgVolume, nil
 }
 
 func main() {
