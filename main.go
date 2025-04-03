52ed1cc68cc543214251e3fb562fd41850750105
Tyler Olson <tolson519@gmail.com>
2025-04-03 02:14:10 -0700

    added output formatting and -debug flag to show debug logs

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"strings"
+	"text/tabwriter"
 )
 
 const API = "https://api.warframe.market/v1"
@@ -21,12 +24,52 @@ type Program struct {
 	items map[string]ItemStat
 }
 
-func newProgram() *Program {
+func NewProgram() *Program {
 	return &Program{
 		items: make(map[string]ItemStat),
 	}
 }
 
+func FormatItems(items map[string]ItemStat) string {
+	maxName, maxPrice, maxVol := 0, 0, 0
+	for _, item := range items {
+		if len(item.name) > maxName {
+			maxName = len(item.name)
+		}
+
+		priceStr := fmt.Sprintf("%.2f", item.weightedAvgPrice)
+		if len(priceStr) > maxPrice {
+			maxPrice = len(priceStr)
+		}
+
+		volStr := fmt.Sprintf("%.2f", item.avgVol)
+		if len(volStr) > maxVol {
+			maxVol = len(volStr)
+		}
+	}
+
+	var b strings.Builder
+
+	w := tabwriter.NewWriter(&b, 0, 0, 2, ' ', 0)
+	fmt.Fprintln(w, "Name\tPrice\tVolume")
+
+	fmt.Fprintln(w, strings.Repeat("-", maxPrice)+"\t"+strings.Repeat("-", maxPrice)+"\t"+strings.Repeat("-", maxVol))
+
+	count := 0
+	for _, item := range items {
+		fmt.Fprintf(w, "%s\t%.2f\t%.2f", item.name, item.weightedAvgPrice, item.avgVol)
+		count++
+		if count != len(items) {
+			fmt.Fprintln(w)
+		}
+	}
+	w.Flush()
+
+	return b.String()
+}
+
+// getVendorStats takes in a list of [Item] which contains the item name and type (mod, weapon, etc).
+// It will then call another function to fetch the api, and save the result to our program.
 func (p *Program) getVendorStats(items []Item) error {
 	for _, v := range items {
 		price, vol, err := getStatisitics(v)
@@ -40,15 +83,22 @@ func (p *Program) getVendorStats(items []Item) error {
 			avgVol:           vol,
 		}
 
-		slog.Info("found item", "name", v.Name, "weightedAvgPrice", price, "avgVol", vol)
+		slog.Debug("found item", "name", v.Name, "weightedAvgPrice", price, "avgVol", vol)
 	}
+
+	slog.Debug("Finished fetching items")
 
 	return nil
 }
 
-// getStatisitics returns (weightedAveragePrice, averageVolume, error)
+// getStatisitics takes in an [Item] containing an items name and [ItemType].
+// It fetches the API statistics and calculates and returns:
+//
+//  1. weightedAveragePrice = ((todayAvgPrice * todayVolume) + (yesterdayAvgPrice * yesterdayVolume)) / (todayVolume + yesterdayVolume)
+//  2. avgVolume = (todayVolume + yesterdayVolume) / 2
+//  3. error
 func getStatisitics(item Item) (float64, float64, error) {
-	slog.Info("requesting", "name", item.Name, "type", item.Type)
+	slog.Debug("requesting", "name", item.Name, "type", item.Type)
 
 	resp, err := http.Get(fmt.Sprintf("%v/items/%v/statistics", API, item.Name))
 	if err != nil {
@@ -123,17 +173,27 @@ func getStatisitics(item Item) (float64, float64, error) {
 }
 
 func main() {
-	// Create a logger
+	debugSet := flag.Bool("debug", false, "a bool")
+	flag.Parse()
+
+	level := slog.LevelInfo
+	if *debugSet {
+		level = slog.LevelDebug
+	}
+
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level: slog.LevelDebug,
+		Level: level,
 	}))
 
 	slog.SetDefault(logger)
 
-	p := newProgram()
-	err := p.getVendorStats(AribiterOfHexis)
+	p := NewProgram()
 
+	err := p.getVendorStats(AribiterOfHexis[:5])
 	if err != nil {
-		slog.Error("error", "error", err)
+		slog.Error("failed to get vendor statistics", "error", err)
 	}
+
+	fmt.Println("Items:")
+	fmt.Println(FormatItems(p.items))
 }
